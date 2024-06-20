5e4a09d402d725d0b21813dccb698c5f9ec41e30
acmb <48370302+acmb@users.noreply.github.com>
2024-06-20 16:16:38 +0100

    Create add-test.js

---
+++ spec/tests/add-test.js
@@ -0,0 +1,44 @@
+var queue
+var name = 'test'
+
+setup(async function ({ $ }) {
+  queue = require('../../index.js')({ db: $.db })
+  await $.db(name).delete()
+})
+
+it('should add', async ({ $, t }) => {
+  var result = await queue.add()
+  t.equal(result, null)
+
+  result = await queue.add('')
+  t.equal(result, null)
+
+  result = await $.db(name).find()
+  t.equal(result.length, 0)
+
+  var data = { from: 'from', to: 'to' }
+  var options = { start: 'now', repeat: 'every tuesday at 12' }
+
+  result = await queue.add(name, data, options)
+
+  t.equal(Object.keys(result).length, 6)
+  t.equal(typeof result.id, 'string')
+  t.equal(result.status.length, 1)
+  t.deepStrictEqual(result.payload, data)
+  t.deepStrictEqual(result.options, options)
+  t.equal(typeof result.created_at.getTime, 'function')
+  t.equal(typeof result.updated_at.getTime, 'function')
+
+  result = await $.db(name).find()
+  t.equal(result.length, 1)
+
+  result = result[0]
+
+  t.equal(Object.keys(result).length, 6)
+  t.equal(typeof result.id, 'string')
+  t.equal(result.status.length, 1)
+  t.deepStrictEqual(result.payload, data)
+  t.deepStrictEqual(result.options, options)
+  t.equal(typeof result.created_at.getTime, 'function')
+  t.equal(typeof result.updated_at.getTime, 'function')
+})
