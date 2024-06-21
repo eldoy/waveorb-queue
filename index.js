3b2d64ba5b47a26f3ab7a5530e96b3a6676f15c2
acmb <48370302+acmb@users.noreply.github.com>
2024-06-21 21:44:55 +0100

    Add listen draft

--- index.js
+++ index.js
@@ -1,20 +1,142 @@
 var util = require('./lib/util.js')
 
-module.exports = function (config = {}) {
-  var { db } = config
+var SECOND = 1000
+
+function getStatus(status, options, error) {
+  var schedule
+  if (status == 'enqueued') {
+    schedule = util.parseSchedule(options)
+  }
+
   return {
-    add: function (name, data, options = {}) {
-      if (!name) return null
+    status,
+    timestamp: new Date(),
+    schedule,
+    error: error?.message
+  }
+}
 
-      var next = util.parseSchedule(options)
+module.exports = function (config = {}) {
+  var { db, interval = 10 } = config
 
-      var enqueued = {
-        status: 'enqueued',
-        timestamp: new Date(),
-        schedule: next
+  function log(message, collection, id) {
+    if (collection) {
+      var now = new Date()
+      var info = `timestamp: ${now.toISOString()}, collection: ${collection}`
+      if (id) {
+        info = `${info}, id: ${id}`
       }
+      console.log(`${message} (${info})`)
+    } else {
+      console.log(message)
+    }
+  }
 
-      return db(name).create({ status: [enqueued], payload: data, options })
+  async function addStatus(collection, jobId, status, options, error) {
+    return db.base.collection(collection).findOneAndUpdate(
+      { _id: jobId },
+      {
+        $push: {
+          status: {
+            $each: [getStatus(status, options, error)],
+            $position: 0
+          }
+        }
+      }
+    )
+  }
+
+  async function addJob(collection, source, scheduled = false) {
+    if (!collection) return
+    log('🛠  Adding job', collection)
+
+    var options = scheduled ? { repeat: source.options.repeat } : source.options
+    var enqueued = getStatus('enqueued', options)
+
+    return db(collection).create({
+      sourceId: source.id || source._id,
+      source,
+      status: [enqueued]
+    })
+  }
+
+  async function processJob(callback, { jobId, collection, source }) {
+    log('🔥 Processing job', collection, jobId)
+
+    var { payload, options } = source
+
+    await addStatus(collection, jobId, 'processing', options)
+
+    try {
+      await callback(payload, options)
+      await addStatus(collection, jobId, 'processed', options)
+      log('✅ Processed job', collection, jobId)
+    } catch (err) {
+      await addStatus(collection, jobId, 'failed', options, err)
+      log('❌ Failed job', collection, jobId)
+    }
+
+    if (options.repeat) {
+      await addJob(collection, source, true)
+    }
+  }
+
+  return {
+    add: async function (name, data, options = {}) {
+      if (!name) return
+      var collection = `${name}-job`
+
+      var result = await db(name).create({ payload: data, options })
+
+      await addJob(collection, result)
+
+      return result
+    },
+    listen: async function (name, callback) {
+      if (!name) return
+      var collection = `${name}-job`
+
+      log(`\n🚀 Listening to ${collection} collection\n`)
+
+      var stream = db.base.collection(collection).watch()
+
+      stream.on('change', async (job) => {
+        if (job.operationType == 'insert') {
+          var { _id, status, source } = job.fullDocument
+          var { status, schedule } = status[0]
+          if (status == 'enqueued' && !schedule) {
+            log('📥 New unscheduled job detected', collection, _id)
+            await processJob(callback, {
+              jobId: _id,
+              collection,
+              source
+            })
+          }
+        }
+      })
+
+      setInterval(async function () {
+        var jobs = await db(collection).find({
+          'status.0.status': 'enqueued',
+          'status.0.schedule': { $lte: new Date() }
+        })
+
+        log(
+          `⏰ Looking for scheduled jobs: found ${jobs.length}`,
+          collection,
+          jobs.map(({ id }) => id).toString()
+        )
+
+        await Promise.all(
+          jobs.map(async ({ id, source }) =>
+            processJob(callback, {
+              jobId: id,
+              collection,
+              source
+            })
+          )
+        )
+      }, interval * SECOND)
     }
   }
 }
