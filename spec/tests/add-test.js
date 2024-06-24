1d7320f6e3c7791e00c3b0b3c4131fac25409015
acmb <48370302+acmb@users.noreply.github.com>
2024-06-24 20:37:27 +0100

    Update add-test.js

--- spec/tests/add-test.js
+++ spec/tests/add-test.js
@@ -1,42 +1,41 @@
-var queue
-var name = 'test'
+var queue = 'test'
 
 setup(async function ({ $ }) {
-  queue = require('../../index.js')({ db: $.db })
-  await $.db(name).delete()
+  await $.db('job').delete()
 })
 
-it('should add', async ({ $, t }) => {
-  var result = await queue.add()
+it('should add job to queue', async ({ $, t }) => {
+  var result = await $.queue.add()
   t.equal(result, null)
 
-  result = await queue.add('')
+  result = await $.queue.add('')
   t.equal(result, null)
 
-  result = await $.db(name).find()
+  result = await $.db('job').find()
   t.equal(result.length, 0)
 
   var data = { from: 'from', to: 'to' }
   var options = { start: 'now', repeat: 'every tuesday at 12' }
+  result = await $.queue.add(queue, data, options)
 
-  result = await queue.add(name, data, options)
-
-  t.equal(Object.keys(result).length, 5)
+  t.equal(Object.keys(result).length, 7)
   t.equal(typeof result.id, 'string')
+  t.equal(result.name, queue)
   t.deepStrictEqual(result.payload, data)
   t.deepStrictEqual(result.options, options)
+  t.equal(result.status.length, 1)
+  t.equal(result.status[0].status, 'enqueued')
+  t.equal(typeof result.status[0].schedule.getTime, 'function')
   t.equal(typeof result.created_at.getTime, 'function')
   t.equal(typeof result.updated_at.getTime, 'function')
 
-  result = await $.db(name).find()
-  t.equal(result.length, 1)
+  var jobs = await $.db('job').find()
+  t.equal(jobs.length, 1)
 
-  result = result[0]
+  t.deepStrictEqual(result, jobs[0])
 
-  t.equal(Object.keys(result).length, 5)
-  t.equal(typeof result.id, 'string')
-  t.deepStrictEqual(result.payload, data)
-  t.deepStrictEqual(result.options, options)
-  t.equal(typeof result.created_at.getTime, 'function')
-  t.equal(typeof result.updated_at.getTime, 'function')
+  t.rejects(
+    async () => $.queue.add(queue, data, options),
+    new Error('Job already exists')
+  )
 })
