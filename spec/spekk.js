07bf615d11cff54e15af35a4972fdb3fe18dd45b
acmb <48370302+acmb@users.noreply.github.com>
2024-06-24 20:37:13 +0100

    Update spekk.js

--- spec/spekk.js
+++ spec/spekk.js
@@ -2,9 +2,11 @@ var mongowave = require('mongowave')
 
 module.exports = async function () {
   var db = await mongowave('waveorb-queue-test')
+  var queue = require('../index.js')({ db, silent: true })
 
   var $ = {
     db,
+    queue,
     params: {},
     app: {
       config: {
