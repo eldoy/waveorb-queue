191b179ba1b8e1a094dcedfeb1bed710ceb3c4a5
Vidar Eldøy <vidar@eldoy.com>
2024-06-30 17:40:51 +0200

    Rename db

--- spec/spekk.js
+++ spec/spekk.js
@@ -1,7 +1,7 @@
 var mongowave = require('mongowave')
 
 module.exports = async function () {
-  var db = await mongowave('waveorb-queue-test')
+  var db = await mongowave('waveorb-scheduler-test')
   var queue = require('../index.js')({ db, silent: true })
 
   var $ = {
